be73500ab1f05e9bd1379580da77c450c1f35094
AMiro <aj.miro.b@gmail.com>
2023-03-05 12:24:57 -0500

    added pseudocode and basic HTML

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=13fc8a4828ae1fb9669507e9c2cc39cf
+
+
+
+
+
+
+
+
+/*
+
+
+```
+
+1. Display a search bar form with button.
+2. Get search history from localStorage.
+3. Display past searches as buttons beneath form.
+    a. On click, append cards in the manner of 4c and 4d below.
+4. On submission: 
+    a. Fetch coordinates with weather API.
+    b. Set results to localStorage.
+    c. Append main card w/ current datapoints for coordinates.
+    d. Append 5-day datapoints in separate cards.
+
+*/
