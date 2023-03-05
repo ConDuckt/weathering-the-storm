f7ad251ca83c8119f28c585cbc5d144932626fe5
AMiro <aj.miro.b@gmail.com>
2023-03-05 15:08:46 -0500

    added API fetch requests; ready to start appending elements/data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,45 @@
-// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=13fc8a4828ae1fb9669507e9c2cc39cf
+
+
+let cityForm = document.querySelector("#city-form");
+let searchInput = document.querySelector("#search-input");
+let mainContainer = document.querySelector("#main-card");
+let subContainer = document.querySelector("#under-cards");
+
+
+function displayWeather(event) {
+    event.preventDefault();
+
+    let locationUrl = ("http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput.value + "&appid=13fc8a4828ae1fb9669507e9c2cc39cf");
+       
+        console.log("locationUrl", locationUrl);
+        console.log("searchInput", searchInput.value);
+
+    fetch(locationUrl)
+        .then(function (location) {
+            return location.json();
+        })
+
+        .then(function (coordinates) {
+            let latitude = (coordinates[0].lat);
+            let longitude = (coordinates[0].lon);
+            let weatherUrl =  ("https://api.openweathermap.org/data/2.5/forecast?lat=" +  latitude + "&lon=" + longitude + "&limit=6&appid=13fc8a4828ae1fb9669507e9c2cc39cf");
+
+            console.log("weatherUrl", weatherUrl);
+
+            fetch(weatherUrl)
+                .then(function (weather) {
+                    return weather.json();
+                    })
+
+            .then(function (result) {
+                console.log(result);        
+        });
+
+    })};
 
 
 
-
+cityForm.addEventListener('submit', displayWeather);
 
 
 
@@ -10,7 +47,6 @@
 /*
 
 
-```
 
 1. Display a search bar form with button.
 2. Get search history from localStorage.
