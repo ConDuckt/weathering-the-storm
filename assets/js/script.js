12a47152dcfe8fb9143c0ca3907bb3f3b55e1c1f
AMiro <aj.miro.b@gmail.com>
2023-03-06 15:08:58 -0500

    ID-ing JSON indices to append to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ let cityForm = document.querySelector("#city-form");
 let searchInput = document.querySelector("#search-input");
 let mainContainer = document.querySelector("#main-card");
 let subContainer = document.querySelector("#under-cards");
+let locationIcon = document.querySelector(".weather-icon");
+
 
 function displayWeather(event) {
     event.preventDefault();
@@ -21,7 +23,7 @@ function displayWeather(event) {
         .then(function (coordinates) {
             let latitude = (coordinates[0].lat);
             let longitude = (coordinates[0].lon);
-            let weatherUrl =  ("https://api.openweathermap.org/data/2.5/forecast?lat=" +  latitude + "&lon=" + longitude + "&limit=6&appid=13fc8a4828ae1fb9669507e9c2cc39cf");
+            let weatherUrl =  ("https://api.openweathermap.org/data/2.5/forecast?lat=" +  latitude + "&lon=" + longitude + "&units=imperial&appid=13fc8a4828ae1fb9669507e9c2cc39cf");
 
             console.log("weatherUrl", weatherUrl);
 
@@ -31,10 +33,9 @@ function displayWeather(event) {
                     })
 
             .then(function (result) {
-                console.log(result);
-                
+
                 if (!document.getElementById(searchInput.value)) {
-                    var button = document.createElement("button");
+                    let button = document.createElement("button");
                     button.id = searchInput.value;
                     button.value = searchInput.value;
                     button.textContent = searchInput.value;
@@ -42,9 +43,65 @@ function displayWeather(event) {
             
                     cityForm.appendChild(button);
 
-            };
-        });
-    })};
+                    let currentWeather = [
+                        (result.city.name),
+                        (result.list[0].dt_txt),
+                        ("Temperature: " + result.list[0].main.temp + "°F"),
+                        ("Humidity: " + result.list[0].main.humidity + "%"),
+                        ("Windspeed: " + result.list[0].wind.speed + "mi/h")
+                    ];
+
+                    let dayTwo = [
+                        (result.city.name),
+                        (result.list[7].dt_txt),
+                        ("Temperature: " + result.list[7].main.temp + "°F"),
+                        ("Humidity: " + result.list[7].main.humidity + "%"),
+                        ("Windspeed: " + result.list[7].wind.speed + "mi/h")
+                    ];
+
+                    let dayThree = [
+                        (result.city.name),
+                        (result.list[15].dt_txt),
+                        ("Temperature: " + result.list[15].main.temp + "°F"),
+                        ("Humidity: " + result.list[15].main.humidity + "%"),
+                        ("Windspeed: " + result.list[15].wind.speed + "mi/h")
+                    ];
+
+                    let dayFour = [
+                        (result.city.name),
+                        (result.list[23].dt_txt),
+                        ("Temperature: " + result.list[23].main.temp + "°F"),
+                        ("Humidity: " + result.list[23].main.humidity + "%"),
+                        ("Windspeed: " + result.list[23].wind.speed + "mi/h")
+                    ];
+
+                    let dayFive = [
+                        (result.city.name),
+                        (result.list[31].dt_txt),
+                        ("Temperature: " + result.list[31].main.temp + "°F"),
+                        ("Humidity: " + result.list[31].main.humidity + "%"),
+                        ("Windspeed: " + result.list[31].wind.speed + "mi/h")
+                    ];
+                    
+                    let daySix = [
+                        (result.city.name),
+                        (result.list[39].dt_txt),
+                        ("Temperature: " + result.list[39].main.temp + "°F"),
+                        ("Humidity: " + result.list[39].main.humidity + "%"),
+                        ("Windspeed: " + result.list[39].wind.speed + "mi/h")
+                    ];
+
+                
+                    console.log(currentWeather);
+                    console.log(dayTwo);
+                    console.log(dayThree);
+                    console.log(dayFour);
+                    console.log(dayFive);
+                    console.log(daySix);
+
+                  }});
+            })};
+
 
 
 cityForm.addEventListener('submit', displayWeather);
