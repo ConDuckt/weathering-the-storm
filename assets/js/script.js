31e46ba96a7941de5c67e5a5e0f27826aa805c39
AMiro <aj.miro.b@gmail.com>
2023-03-06 16:44:32 -0500

    all buttons and appends (technically) working

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,21 @@ let searchInput = document.querySelector("#search-input");
 let mainContainer = document.querySelector("#main-card");
 let subContainer = document.querySelector("#under-cards");
 let locationIcon = document.querySelector(".weather-icon");
+
 // webapp main function
 function displayWeather(event) {
     event.preventDefault();
-// getting a city's coordinates via an initial API call
+
+    // getting a city's coordinates via an initial API call
     let locationUrl = ("http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput.value + "&appid=13fc8a4828ae1fb9669507e9c2cc39cf");
 
     fetch(locationUrl)
         .then(function (location) {
             return location.json();
         })
-// plugging the coordinates into a second API call
+
+        // plugging the coordinates from first API call
+        // into a second API call for actual weather data
         .then(function (coordinates) {
             let latitude = (coordinates[0].lat);
             let longitude = (coordinates[0].lon);
@@ -23,19 +27,35 @@ function displayWeather(event) {
             fetch(weatherUrl)
                 .then(function (weather) {
                     return weather.json();
-                    })
-// using the second call JSON
-            .then(function (result) {
-// creating a button for new cities entered into form
-                if (!document.getElementById(searchInput.value)) {
-                    let button = document.createElement("button");
-                    button.id = searchInput.value;
-                    button.value = searchInput.value;
-                    button.textContent = searchInput.value;
-                    button.onclick = event;
-            
-                    cityForm.appendChild(button);
-// getting JSON data for current weather
+                })
+
+                .then(function (result) {
+
+                    // clearing any existing container text
+                    mainContainer.innerHTML = "";
+                    subContainer.innerHTML = "";
+
+                    // separated appended button functionality
+                    // from displayWeather(event) so they remain
+                    // funtional
+                    function buttonClick(clickAgain) {
+                        let button = clickAgain.target;
+                        let searchTerm = button.value;
+                        searchInput.value = searchTerm;
+                        displayWeather(clickAgain);
+                    };
+
+                    if (!document.getElementById(searchInput.value)) {
+                        let button = document.createElement("button");
+                        button.id = searchInput.value;
+                        button.value = searchInput.value;
+                        button.textContent = searchInput.value;
+                        button.onclick = buttonClick;
+                
+                        cityForm.appendChild(button);
+                    };
+
+                    // setting JSON data for current weather
                     let currentWeather = [
                         (result.city.name),
                         (result.list[0].dt_txt),
@@ -43,10 +63,20 @@ function displayWeather(event) {
                         ("Humidity: " + result.list[0].main.humidity + "%"),
                         ("Windspeed: " + result.list[0].wind.speed + "mi/h")
                     ];
-// getting JSON data for 5-day forecast
+
+                    for (let item of currentWeather) {
+                        let listItem = document.createElement("li");
+                        let textNode = document.createTextNode(item);
+                        listItem.appendChild(textNode);
+                        mainContainer.appendChild(listItem);
+                    };
+
+                    // setting JSON data for 5-day forecast
+                    // using for loop to specify every 8th indexed 
+                    // item since JSON returns 8 timestamps/day
                     let forecastArray = [];
-// using for loop to specify every 8th indexed item since JSON returns 8 timestamps/day
-                    for (var i = 7; i < result.list.length; i+=8) {
+
+                    for (let i = 7; i < result.list.length; i+=8) {
                         forecastArray.push([
                             (result.city.name),
                             (result.list[i].dt_txt),
@@ -55,17 +85,18 @@ function displayWeather(event) {
                             ("Windspeed: " + result.list[i].wind.speed + "mi/h"),
                         ]);
                     };
-                
-                    console.log(currentWeather);
-                    console.log(forecastArray);
 
-                  }});
-            })};
+                    for (let item of forecastArray) {
+                        let listItem = document.createElement("li");
+                        let textNode = document.createTextNode(item);
+                        listItem.appendChild(textNode);
+                        subContainer.appendChild(listItem);
+                    };
 
-
+            });
+})};
 
 cityForm.addEventListener('submit', displayWeather);
-
 
 
 
